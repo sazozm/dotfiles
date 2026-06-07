b5faaafbd40765f06fedda0879eef892f1625223
sazozm <maksimnovogrodskij.com>
2026-06-07 16:17:16 +0300

    some fixes

---
+++ nvim-def/.config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "storm", -- варианты: storm, moon, night, day
+      transparent = true, -- если нужен прозрачный фон
+    },
+  },
+}
--- nvim-def/.config/nvim/lua/plugins/treesitter.lua
+++ nvim-def/.config/nvim/lua/plugins/treesitter.lua
@@ -13,7 +13,7 @@ return {
     treesitter = {
       highlight = true, -- enable/disable treesitter based highlighting
       indent = true, -- enable/disable treesitter based indentation
-      auto_install = false, -- enable/disable automatic installation of detected languages
+      auto_install = true, -- enable/disable automatic installation of detected languages
       ensure_installed = {
         "lua",
         "vim",
